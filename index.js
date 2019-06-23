ef9991862ed49e8871e691f1e5a25dbaeed01103
jandrewreel <ree15008@byui.edu>
2019-06-22 19:34:47 -0600

    package update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 // const port = process.env.PORT || 5000;
 
 // tell it to use the public directory as one where static files live
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + 'public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -23,10 +23,22 @@ app.set('view engine', 'ejs');
 
 // set up a rule that says requests to "/math" should be handled by the
 // handleMath function below
-//app.get('/math', handleMath);
+app.get('/math', handleMath);
 
 // start the server listening
 app.listen(port, function() {
   console.log('Node app is running on port', port);
   console.log('Waiting for response...');
 });
+
+
+/*******************************************
+* Program Functions
+*******************************************/
+function handleMath(req, res) {
+   const mailType = request.query.mailType;
+   const weight = request.query.weight;
+
+   console.log('Mail Type: ' + mailType);
+   console.log('Weight: ' + weight);
+}
