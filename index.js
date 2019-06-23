f980f5ba53b210479543d0e4808762f2b883f729
jandrewreel <ree15008@byui.edu>
2019-06-22 19:30:42 -0600

    package update

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+//const path = require('path')
+const port = process.env.PORT || 5000
+const app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+// const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// tell it to use the public directory as one where static files live
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// set up a rule that says requests to "/math" should be handled by the
+// handleMath function below
+//app.get('/math', handleMath);
+
+// start the server listening
+app.listen(port, function() {
+  console.log('Node app is running on port', port);
+  console.log('Waiting for response...');
+});
