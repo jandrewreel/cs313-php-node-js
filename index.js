6fa9a02e1324d648e6440a56de9884fa3b14e845
jandrewreel <ree15008@byui.edu>
2019-06-22 19:39:55 -0600

    package update

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-//const path = require('path')
+const path = require('path')
 const port = process.env.PORT || 5000
 const app = express();
 
 // express()
-//   .use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 //   .set('views', path.join(__dirname, 'views'))
 //   .set('view engine', 'ejs')
 //   .get('/', (req, res) => res.render('pages/index'))
@@ -15,7 +15,7 @@ const app = express();
 // const port = process.env.PORT || 5000;
 
 // tell it to use the public directory as one where static files live
-app.use(express.static(__dirname + 'public'));
+//app.use(express.static(__dirname + 'public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -23,7 +23,7 @@ app.set('view engine', 'ejs');
 
 // set up a rule that says requests to "/math" should be handled by the
 // handleMath function below
-app.get('/math', handleMath);
+//app.get('/math', handleMath);
 
 // start the server listening
 app.listen(port, function() {
@@ -35,10 +35,10 @@ app.listen(port, function() {
 /*******************************************
 * Program Functions
 *******************************************/
-function handleMath(req, res) {
-   const mailType = request.query.mailType;
-   const weight = request.query.weight;
+// function handleMath(req, res) {
+//    const mailType = request.query.mailType;
+//    const weight = request.query.weight;
 
-   console.log('Mail Type: ' + mailType);
-   console.log('Weight: ' + weight);
-}
+//    console.log('Mail Type: ' + mailType);
+//    console.log('Weight: ' + weight);
+// }
